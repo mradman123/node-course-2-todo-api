4dd015b76c11ca836b098b6bf4f9c3dfcb4d01e8
Makala-PC\Makala <mradman123@gmail.com>
2017-04-07 12:25:53 +0200

    Test POST /todos route

--- server/server.js
+++ server/server.js
@@ -10,10 +10,10 @@ var app = express();
 app.use(bodyParser.json());
 
 app.post('/todos', (req, res) => {
-  console.log(req.body);
   var todo = new Todo({
     text: req.body.text
   });
+  
   todo.save().then((doc) =>{
     res.send(doc);
   }, (e) => {
@@ -24,3 +24,5 @@ app.post('/todos', (req, res) => {
 app.listen(3001, () => {
   console.log('Started on port 3001');
 });
+
+module.exports = {app};
