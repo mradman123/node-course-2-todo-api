99656cfd016e357e45f6d4fd230c54e740487166
Makala-PC\Makala <mradman123@gmail.com>
2017-04-21 14:58:59 +0200

    Test the DELETE /todos/:id routes

--- server/server.js
+++ server/server.js
@@ -57,7 +57,7 @@ app.delete('/todos/:id', (req,res) => {
     if(!todo){
        return res.status(404).send();
     }
-    res.status(200).send(todo);
+    res.status(200).send({todo});
   }, (e) => res.status(400).send());
 
 });
